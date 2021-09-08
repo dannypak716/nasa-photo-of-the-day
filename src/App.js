50c77eebeef1086a63b0c656f1b7b919ae6b16d8
Jeong Pak <dannypak716@gmail.com>
2021-09-08 15:51:05 -0400

    installed axios, imported axios, set the hook for API and called in the data properly and assigned the data to the empty array in state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [nasaPic, setNasaPic] = useState([]);
+
+  useEffect( () => {
+    axios.get ('https://api.nasa.gov/planetary/apod?api_key=ZtuxhRNJqMmPL1ZUidSK3B8xjHrMdU3G9XYvTbxi')
+    .then ( res => {
+      setNasaPic(res.data);
+    }).catch( err => console.log(err));
+  }, [])
+
   return (
     <div className="App">
       <p>
